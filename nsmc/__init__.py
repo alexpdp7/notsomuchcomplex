581123969110b592aa42b9f628d9304858a363d8
alex <alex@pdp7.net>
2022-08-28 17:44:38 +0000

    Add notmuch + Yahoo support

--- nsmc/__init__.py
+++ nsmc/__init__.py
@@ -31,6 +31,11 @@ class EmailAccount:
         home = os.environ["HOME"]
         self.store_path = pathlib.Path(f"{home}/.mail/{login}")
 
+    def get_proxies(self):
+        return []
+
+    def get_proxy_config(self, redirect_port):
+        return ""
 
 @dataclasses.dataclass(frozen=True)
 class Proxy:
@@ -45,26 +50,22 @@ class Proxy:
             server_port = {self.server_port}
         """)
 
-        
-class GmailAccount(EmailAccount):
-    def __init__(self, login):
-        self.redirect_port = get_free_port()
-        super().__init__(login)
 
+class GmailAccount(EmailAccount):
     def get_proxies(self):
         return [
             Proxy("IMAP", "imap.gmail.com", 993),
             Proxy("SMTP", "smtp.gmail.com", 465),
         ]
 
-    def get_proxy_config(self):
+    def get_proxy_config(self, redirect_port):
         return _u(f"""
             [{self.login}]
 
             permission_url = https://accounts.google.com/o/oauth2/auth
             token_url = https://oauth2.googleapis.com/token
             oauth2_scope = https://mail.google.com/
-            redirect_uri = http://localhost:{self.redirect_port}
+            redirect_uri = http://localhost:{redirect_port}
             # https://gitlab.gnome.org/GNOME/gnome-online-accounts/-/blob/master/meson_options.txt#L18
             client_id = 44438659992-7kgjeitenc16ssihbtdjbgguch7ju55s.apps.googleusercontent.com
             client_secret = -gMLuQyDiI0XrQS_vx_mhuYF
@@ -103,20 +104,62 @@ class GmailAccount(EmailAccount):
             # Expunge Both
             # Save the synchronization state files in the relevant directory
             SyncState *
+
         """)
 
-def create_email_account(login):
-    if login.endswith("gmail.com"):
-        return GmailAccount(login)
+
+class YahooAccount(EmailAccount):
+    def __init__(self, login, options):
+        self.application_password = options["application_password"]
+        super().__init__(login)
+
+    def get_mbsync_config(self, _proxies_to_ports):
+        return _u(f"""
+            IMAPAccount {self.login}
+            # Address to connect to
+            Host imap.mail.yahoo.com
+            User {self.login}
+            Pass {self.application_password}
+            SSLType IMAPS
+            PipelineDepth 5
+
+            IMAPStore {self.login}-remote
+            Account {self.login}
+
+            MaildirStore {self.login}-local
+            SubFolders Verbatim
+            # The trailing "/" is important
+            Path {self.store_path}/
+            Inbox {self.store_path}/Inbox
+
+            Channel {self.login}
+            Far :{self.login}-remote:
+            Near :{self.login}-local:
+            Patterns *
+            # Automatically create missing mailboxes, both locally and on the server
+            Create Both
+            # Sync the movement of messages between folders and deletions, add after making sure the sync works
+            # Expunge Both
+            # Save the synchronization state files in the relevant directory
+            # SyncState *
+
+        """)
+
+
+def create_email_account(address, options):
+    if address.endswith("@gmail.com"):
+        return GmailAccount(address)
+    if address.endswith("@yahoo.es"):
+        return YahooAccount(address, options)
     assert False, f"unknown type for login {login}"
 
 
-def create_emailproxy_config(proxies_to_ports, emails):
+def create_emailproxy_config(proxies_to_ports, emails, redirect_port):
     config = ""
     for proxy, port in proxies_to_ports.items():
         config += proxy.get_proxy_config_port(port)
     for email in emails:
-        config += email.get_proxy_config()
+        config += email.get_proxy_config(redirect_port)
     return config
 
 
@@ -131,22 +174,26 @@ def main():
     config_dir = pathlib.Path(appdirs.user_config_dir(APPNAME))
     with open(config_dir / "accounts.yaml") as config_file:
         config = yaml.safe_load(config_file)
-    emails = [create_email_account(login) for login in config]
+    redirect_port = config["redirect_port"]
+    emails = [create_email_account(address, options) for address, options in config["emails"].items()]
     proxies = set(itertools.chain(*[email.get_proxies() for email in emails]))
     proxies_to_ports = dict([(proxy, get_free_port()) for proxy in proxies])
     epc_path = config_dir / "emailproxy.config" 
     with open(epc_path, "w", encoding="utf8") as epc_file:
-        epc_file.write(create_emailproxy_config(proxies_to_ports, emails))
+        epc_file.write(create_emailproxy_config(proxies_to_ports, emails, redirect_port))
     mb_path = config_dir / "mbsyncrc"
     with open(mb_path, "w", encoding="utf8") as mb_file:
         mb_file.write(create_mbsync_config(emails, proxies_to_ports))
     for email in emails:
         email.store_path.mkdir(parents=True, exist_ok=True)
-        print(f"{email.login} redirect proxy port {email.redirect_port}")
     with subprocess.Popen(["emailproxy", "--no-gui", "--local-server-auth", f"--config-file={epc_path}"], env={"PYSTRAY_BACKEND": "dummy", "PATH": os.environ["PATH"]}) as ep_process:
         time.sleep(5)  # TODO: wait proxy
-        subprocess.run(["mbsync", "-a", "-c", mb_path])
-        ep_process.terminate()
+        while True:
+            print("Running mbsync...")
+            subprocess.run(["mbsync", "-a", "-c", mb_path])
+            print("... mbsync finished, going to sleep")
+            subprocess.run(["notmuch", "new"])
+            time.sleep(60)
 
 
 if __name__ == "__main__":
